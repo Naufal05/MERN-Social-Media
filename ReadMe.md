904caa34566e412ec3257d83df39fbcdf1f3dd0c
Mohamed Naufal <mnaufalmz@gmail.com>
2024-04-21 06:54:44 +0530

    route initial

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+//here there is no need to use app, insteaad we are using Router()
+
+
+// END POINTS
+// app.get("/api/test", (req, res) => {
+//     console.log("test is successful");
+//     res.send("Test is successful");
+// });
--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require("express");
 const app = express()
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const userRoute = require("./routes/user") // Corrected path
 
 dotenv.config()
 
-mongoose.connect("mongodb+srv://user:Sunday_1989@cluster0.pfhzac7.mongodb.net/").then(() => console.log("DB connection successful")).catch((err)=> console.log(err))
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("DB connection successful"))
+.catch((err)=> console.log(err))
 
-app.listen(5000, ()=> {
+
+app.use("/api/user", userRoute);
+
+
+app.listen(process.env.PORT || 5000, ()=> {
     console.log("Backend server is running")
 })
